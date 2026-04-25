3026bb1673dfbb4421d8e1a185229ea0009cf4d6
kawzaki <kawzaki@users.noreply.github.com>
2026-04-25 20:11:07 +0300

    Fix: Relative asset paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/qatarim/', // Required for GitHub Pages /repository-name/
+  base: './', // Using relative paths to avoid sub-folder mapping issues
 })
